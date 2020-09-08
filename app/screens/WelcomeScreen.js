c2d46562000c47c8500e8dbe883ea3f7cba6d9be
Or Saada <orsaa@post.bgu.ac.il>
2020-09-08 21:18:32 +0300

    finish tutorial

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,17 +1,45 @@
 import React from "react";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
 
-function WelcomeScreen() {
+export function WelcomeScreen(props) {
   return (
     <ImageBackground
       source={require("../assets/background.jpg")}
       style={styles.backgound}
-    ></ImageBackground>
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/ituran.png")} />
+        <Text>ITURAN APP</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   backgound: {
     flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#dc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+  logo: {
+    width: 150,
+    height: 80,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 80,
+    alignItems: "center",
   },
 });
